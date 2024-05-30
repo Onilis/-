a271abf803a49dad0d93108738b295d7ca78f274
Onilis <166647021+Onilis@users.noreply.github.com>
2024-05-30 21:57:30 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,48 +1,53 @@
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите число или строку:");
-        Scanner sca = new Scanner(System.in);
-        String exp = sca.nextLine();
+        System.out.println("Введите выражение");
+
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
         char action;
-        String [] data;
-        if (exp.length() > 10) {
-            throw new Exception("Вводная строка не должна быть длиннее 10 символов");
-        }
-        if (exp.contains("+")) {
-            data = exp.split("\\+");
+        String[] data;
+        if (exp.contains(" + ")) {
+            data = exp.split(" \\+ ");
             action = '+';
-        } else if (exp.contains("-")) {
-            data = exp.split("\\ -");
+        } else if (exp.contains(" - ")) {
+            data = exp.split(" - ");
             action = '-';
-        } else if (exp.contains("*")) {
-            data = exp.split("\\*");
+        } else if (exp.contains(" * ")) {
+            data = exp.split(" \\* ");
             action = '*';
-        } else if (exp.contains("/")) {
-            data = exp.split("\\/");
+        } else if (exp.contains(" / ")) {
+            data = exp.split(" / ");
             action = '/';
-        }else {
-            throw new Exception("Не корректный знак");
+        } else {
+            throw new Exception("Некорректный знак действия");
         }
         if (action == '*' || action == '/') {
-            if (data[1].contains("\"")) throw new Exception("Делить или умножать можно только на число");
+            if (data[1].contains("\"")) throw new Exception("Строку можно делить или умножать только на число");
         }
         for (int i = 0; i < data.length; i++) {
             data[i] = data[i].replace("\"", "");
         }
 
+
         if (action == '+') {
             printInQuotes(data[0] + data[1]);
-        } else if (action == '*') {
+        } else if (action == '*')
+        {
             int multiplier = Integer.parseInt(data[1]);
             String result = "";
+
+
             for (int i = 0; i < multiplier; i++) {
                 result += data[0];
             }
             printInQuotes(result);
-        }else if (action == '-') {
+
+
+        } else if (action == '-') {
             int index = data[0].indexOf(data[1]);
-            if(index == -1) {
+            if (index == -1) {
                 printInQuotes(data[0]);
             } else {
                 String result = data[0].substring(0, index);
@@ -50,13 +55,18 @@ public class Calculator {
                 printInQuotes(result);
             }
         } else {
-            int newLen = data[0].length()/Integer.parseInt((data[1]));
+            int newLen = data[0].length() / Integer.parseInt(data[1]);
             String result = data[0].substring(0, newLen);
             printInQuotes(result);
-            return;
         }
+        if (data[0].length() > 10)
+            throw new Exception("Введено больше 10 символов");
+        if (data[1].length() > 10)
+            throw new Exception("Введено больше 10 символов");
     }
-    static void printInQuotes(String text) {
-        System.out.println("\""+text+"\"");
-    }
+
+    static void printInQuotes(String text){
+        System.out.print(text);
+        if (text.length()>40)
+            System.out.println(" "+text.substring(0,40)+"..."+" ");}
 }
